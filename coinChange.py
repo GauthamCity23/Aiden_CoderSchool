ae2eafe7e916deaf7fc1912233242fee3eea165b
aidendwayneto-cell <aidendwayneto@gmail.com>
2026-01-10 12:08:44 -0500

    Create coinChange.py

---
+++ coinChange.py
@@ -0,0 +1,18 @@
+number = int(input("Please enter a number between 50 and 150: "))
+leftover = 0
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
+qused = 0
+dused = 0
+nused = 0
+pused = 0
+qused = number//quarter
+leftover = number-qused*quarter
+dused = leftover//dime
+leftover = leftover-dime*duesed
+nused = leftover// nickel
+leftover = leftover-nused*nickel
+pused = leftover
+#print(str(P:)pused+str(N:)nused+str(D:)duesed+str(Q:)quesed)
